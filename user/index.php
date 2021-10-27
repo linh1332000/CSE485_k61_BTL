b2ee3a96b399aeee869d27be34386590f0ce51b2
Phạm Quang Huy <80248283+noohuy@users.noreply.github.com>
2021-10-27 15:22:07 +0700

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,3 +1,5 @@
+<?php include('../config/connect.php'); ?>
+
 <!doctype html>
 <html lang="en">
 
@@ -9,20 +11,67 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/user.css">
-    <title>Hello, world!</title>
+    <title>User Info</title>
 </head>
 
 <body>
 
+
     <div class="container bootstrap snippets bootdeys">
         <div class="row" id="user-profile">
+        <?php 
+        
+
+        if(isset($_SESSION['no-user-found']))
+        {
+            echo $_SESSION['no-user-found'];
+            unset($_SESSION['no-user-found']);
+        }
+
+        if(isset($_SESSION['update']))
+        {
+            echo $_SESSION['update'];
+            unset($_SESSION['update']);
+        }
+
+    
+    
+        ?>
+        <?php
+           
+            if (isset($_GET['id_user'])) {
+               
+                $id_user = $_GET['id_user'];
+
+              
+                $sql = "SELECT * FROM user WHERE id_user = $id_user";
+               
+                $res = mysqli_query($conn, $sql);
+               
+                $count = mysqli_num_rows($res);
+               
+                if ($count == 1) {
+                  
+                    $row = mysqli_fetch_assoc($res);
+                    $id_user=$row['id_user'];
+                    $user_name = $row['user_name'];
+                    $user_email = $row['user_email'];
+                    $picture = $row['picture'];
+                    $user_sdt=$row['user_sdt'];
+                    $id_lop=$row['id_lop'];
+                } else {
+               
+                    header('location:' . SITEURL);
+                }
+            } 
+            ?>
             <div class="col-lg-3 col-md-4 col-sm-4">
                 <div class="main-box clearfix">
-                    <h2>John Doe </h2>
-                    
-                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="profile-img img-responsive center-block">
+                    <h2> <?php echo $user_name; ?> </h2>
+
+                    <img src="<?php echo $picture; ?>" alt="" class="profile-img img-responsive center-block">
                     <div class="profile-label">
-                        <span class="label label-danger">Admin</span>
+                        <span class="label label-danger">User</span>
                     </div>
 
                 </div>
@@ -32,7 +81,7 @@
                 <div class="main-box clearfix">
                     <div class="profile-header">
                         <h3><span>User info</span></h3>
-                        <a href="#" class="btn btn-primary edit-profile">
+                        <a href="<?php echo SITEURL; ?>user/update-profile.php?id_user=<?php echo $id_user; ?>" class="btn btn-primary edit-profile">
                             <i class="fa fa-pencil-square fa-lg"></i> Edit profile
                         </a>
                     </div>
@@ -41,45 +90,37 @@
                         <div class="col-sm-8">
                             <div class="profile-user-details clearfix">
                                 <div class="profile-user-details-label">
-                                    First Name
+                                    Name:
                                 </div>
                                 <div class="profile-user-details-value">
-                                    John
+                                    <?php echo $user_name; ?>
                                 </div>
                             </div>
                             <div class="profile-user-details clearfix">
                                 <div class="profile-user-details-label">
-                                    Last Name
+                                    Email:
                                 </div>
                                 <div class="profile-user-details-value">
-                                    Doe
+                                <?php echo $user_email; ?>
                                 </div>
                             </div>
                             <div class="profile-user-details clearfix">
                                 <div class="profile-user-details-label">
-                                    Address
+                                    Phone:
                                 </div>
                                 <div class="profile-user-details-value">
-                                    10880 Malibu Point,
-                                    <br> Malibu, Calif., 90265
+                                <?php echo $user_sdt; ?>
                                 </div>
                             </div>
                             <div class="profile-user-details clearfix">
                                 <div class="profile-user-details-label">
-                                    Email
+                                    Id Class:
                                 </div>
                                 <div class="profile-user-details-value">
-                                    johndoe@bootdey.com
+                                <?php echo $id_lop; ?>
                                 </div>
                             </div>
-                            <div class="profile-user-details clearfix">
-                                <div class="profile-user-details-label">
-                                    Phone number
-                                </div>
-                                <div class="profile-user-details-value">
-                                    011 223 344 556 677
-                                </div>
-                            </div>
+
                         </div>
                     </div>
 
@@ -93,12 +134,13 @@
 
                 </div>
             </div>
+
         </div>
     </div>
- 
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    
+
 </body>
 
 </html>
