627f1c0e311213b387dc91952bb267e2b1fcf32f
dHuy <duyhuy1900@gmail.com>
2021-11-01 11:42:15 +0700

    Update logout.php

--- user/logout.php
+++ user/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start(); //Dịch vụ bảo vệ
-    if(isset($_SESSION['loginOK'])){
-        unset($_SESSION['loginOK']);
-        header("Location: ../index.php");
-    }
+    
+    session_destroy();
+    header("Location: ../index.php");
+    
 ?>
