8ebacd01c9c5edc93dc90f788db5815bf5386f1b
Phạm Quang Huy <80248283+noohuy@users.noreply.github.com>
2021-11-01 18:18:34 +0700

    update index.php

--- user/index.php
+++ user/index.php
@@ -1,5 +1,29 @@
 <?php include('../config/connect.php'); ?>
+<?php
 
+
+session_start();
+if (!isset($_SESSION['loginOK'])) {
+    header("Location:/index.php");
+} else {
+    unset($_SESSION['loginOK']);
+}
+
+
+if (isset($_SESSION['no-user-found'])) {
+    echo $_SESSION['no-user-found'];
+    unset($_SESSION['no-user-found']);
+}
+
+if (isset($_SESSION['update'])) {
+    echo $_SESSION['update'];
+    unset($_SESSION['update']);
+}
+if (isset($_SESSION['upload'])) {
+    echo $_SESSION['upload'];
+    unset($_SESSION['upload']);
+}
+?>
 <!doctype html>
 <html lang="en">
 
@@ -19,29 +43,7 @@
 
     <div class="container bootstrap snippets bootdeys">
         <div class="row" id="user-profile">
-            <?php
-<<<<<<< HEAD
-              
-            if(!isset($_SESSION['loginOK'])){
-                header("Location:/index.php");
-            }
-=======
 
-
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
-            if (isset($_SESSION['no-user-found'])) {
-                echo $_SESSION['no-user-found'];
-                unset($_SESSION['no-user-found']);
-            }
-
-            if (isset($_SESSION['update'])) {
-                echo $_SESSION['update'];
-                unset($_SESSION['update']);
-            }
-
-
-
-            ?>
             <?php
 
             if (isset($_GET['id_user'])) {
@@ -76,10 +78,11 @@
                 <div class="main-box clearfix">
                     <h2> <?php echo $user_name; ?> </h2>
 
-                    <img src="<?php echo $picture; ?>" alt="" class="profile-img img-responsive center-block">
+                    <img src="<?php echo SITEURL; ?>images/<?php echo $picture; ?>" width="100px" alt="" class="profile-img img-responsive center-block">
                     <div class="profile-label">
                         <span class="label label-danger">User</span>
                     </div>
+
 
                 </div>
             </div>
@@ -91,7 +94,7 @@
                         <a href="<?php echo SITEURL; ?>user/update-profile.php?id_user=<?php echo $id_user; ?>" class="btn btn-primary edit-profile">
                             <i class="fa fa-pencil-square fa-lg"></i> Edit profile
                         </a>
-                        <a href="logout.php">Thoát</a>
+                        <a href="logout.php" class="btn btn-primary float-end">Thoát</a>
                     </div>
 
                     <div class="row profile-user-info">
@@ -131,44 +134,27 @@
 
                         </div>
                     </div>
-                    <div class="container-fluid">
+                    <div class="container" style="padding: 0;">
                         <div class="row">
                             <div class="col-4">
                                 <div class="badge bg-primary text-wrap" style="width: 10rem; margin: 5% 20%; font-size: 20px;">
                                     Room
                                 </div>
                                 <?php
-<<<<<<< HEAD
-                               
+
                                 $sql2 = "SELECT * FROM zoom ";
 
-                              
+
                                 $res2 = mysqli_query($conn, $sql2);
 
-                             
-=======
-                                //Create a SQL Query to Get all the Food
-                                $sql2 = "SELECT * FROM zoom ";
 
-                                //Execute the qUery
-                                $res2 = mysqli_query($conn, $sql2);
-
-                                //Count Rows to check whether we have foods or not
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
                                 $count = mysqli_num_rows($res2);
 
 
                                 if ($count > 0) {
-<<<<<<< HEAD
-                                 
+
                                     while ($row = mysqli_fetch_assoc($res2)) {
-                                     
-=======
-                                    //We have food in Database
-                                    //Get the Foods from Database and Display
-                                    while ($row = mysqli_fetch_assoc($res2)) {
-                                        //get the values from individual columns
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
+
                                         $id_zoom = $row['id_zoom'];
                                         $name_z = $row['name_z'];
                                         $id_user = $row['id_user'];
@@ -179,18 +165,14 @@
                                         <ul class="list-group ">
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <?php echo $name_z; ?>
-                                                <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn btn-primary">Tham gia</a>
+                                                <a href="" class="btn btn-primary">Tham gia</a>
                                             </li>
                                         </ul>
 
                                 <?php
                                     }
                                 } else {
-<<<<<<< HEAD
-                                    
-=======
-                                    //Food not Added in Database
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
+
                                     echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
                                 }
 
@@ -206,42 +188,22 @@
                                     </tr>
 
                                     <?php
-<<<<<<< HEAD
-                                 
+
                                     $sql1 = "SELECT * FROM sukien ";
 
-                                  
+
                                     $res1 = mysqli_query($conn, $sql1);
 
-                                   
+
                                     $count = mysqli_num_rows($res1);
 
-                                 
+
                                     $sn = 1;
 
                                     if ($count > 0) {
-                                      
+
                                         while ($row = mysqli_fetch_assoc($res1)) {
-                                         
-=======
-                                    //Create a SQL Query to Get all the Food
-                                    $sql1 = "SELECT * FROM sukien ";
 
-                                    //Execute the qUery
-                                    $res1 = mysqli_query($conn, $sql1);
-
-                                    //Count Rows to check whether we have foods or not
-                                    $count = mysqli_num_rows($res1);
-
-                                    //Create Serial Number VAriable and Set Default VAlue as 1
-                                    $sn = 1;
-
-                                    if ($count > 0) {
-                                        //We have food in Database
-                                        //Get the Foods from Database and Display
-                                        while ($row = mysqli_fetch_assoc($res1)) {
-                                            //get the values from individual columns
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
                                             $sk_id = $row['sk_id'];
                                             $sk_name = $row['sk_name'];
                                             $sk_date = $row['sk_date'];
@@ -249,14 +211,13 @@
 
                                             <tr>
                                                 <td><?php echo $sn++; ?>. </td>
-                                                <td><?php echo $sk_name; ?></td>
+                                                <td>
+                                                    <a href="<?php echo SITEURL; ?>event.php?sk_id=<?php echo $sk_id; ?>" class="link-dark" style="text-decoration: none;"><?php echo $sk_name; ?> </a>
+
+                                                </td>
                                                 <td><?php echo $sk_date; ?></td>
                                                 <td>
-<<<<<<< HEAD
                                                     <a href="<?php echo SITEURL; ?>user/invite.php?sk_id=<?php echo $sk_id; ?>" class="btn btn-primary">Mời</a>
-=======
-                                                    <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn btn-primary">Mời</a>
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
 
                                                 </td>
                                             </tr>
@@ -264,11 +225,7 @@
                                     <?php
                                         }
                                     } else {
-<<<<<<< HEAD
-                                      
-=======
-                                        //Food not Added in Database
->>>>>>> 699850d438d7afdc2a91158379b62c6d91af35f2
+
                                         echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
                                     }
 
