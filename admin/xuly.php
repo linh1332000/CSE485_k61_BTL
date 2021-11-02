e4caf3da5faa4e373f29662069368fca1b3373ab
linh1332000 <78039462+linh1332000@users.noreply.github.com>
2021-11-02 23:18:13 +0700

    Update xuly.php

--- admin/xuly.php
+++ admin/xuly.php
@@ -1,16 +1,18 @@
 <?php include('../config/connect.php'); ?>
 <?php
-        // Dịch vụ bảo vệ:
+// Dịch vụ bảo vệ:
         session_start(); //Công ty dịch vụ Bảo vệ
 
         if(isset($_POST['sbmGuiDi'])){
             $username = $_POST['txtEmail'];
             $password = $_POST['txtPass'];
-           
+            // $password_hash = md5($password);
 
-
+            $username = mysqli_real_escape_string($conn,$username);
+            $password_1 = mysqli_real_escape_string($conn,$password);
+   
             // Bước 02: Truy vấn thông tin
-            $sql = "SELECT * FROM user WHERE user_email='$username' AND user_pass='$password'";
+            $sql = "SELECT * FROM user WHERE user_email='$username' AND user_pass='$password_1' limit 0,1";
             $result = mysqli_query($conn,$sql);
           
             // Bước 03: Xác thực > Đăng nhập > Ở trên, trả về 1 bản ghi thôi
@@ -20,8 +22,9 @@
                 $id_user=$row['id_user'];
                 if ($row['user_lv']==1){
                     $_SESSION['id_user']=$row['id_user'];
-                 
-                   
+                    $_SESSION['user_name']=$row['user_name'];
+                    $_SESSION['id_lop']=$row['id_lop'];
+                    $_SESSION['id_LienKhoa']=$row['id_LienKhoa'];
                     ?>
                     <script>
                         window.alert('Login Success, Welcome Admin!');
@@ -32,9 +35,6 @@
 
                 }
                 else{
-                    $_SESSION['id_user']=$row['id_user'];
-                   
-                   
                     ?>
                     <script>
                         window.location.href='index.php';
