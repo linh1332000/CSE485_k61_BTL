041aa78e1a08f6e246ec7091386449ba8c25acad
Phạm Quang Huy <80248283+noohuy@users.noreply.github.com>
2021-11-02 23:36:57 +0700

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,53 +1,38 @@
 <?php include('../config/connect.php'); ?>
 <?php
-   
-    
-   session_start();
-    if (!isset($_SESSION['loginOK'])) {
-        header("Location:/index.php");
-        
-    }else{
-        unset($_SESSION['loginOK']);
-        
-    }
-   
-    
 
 
-    if (isset($_SESSION['no-user-found'])) {
-        echo $_SESSION['no-user-found'];
-        unset($_SESSION['no-user-found']);
-    }
+session_start();
+if (!isset($_SESSION['loginOK'])) {
+    header("Location:/index.php");
+} else {
+    unset($_SESSION['loginOK']);
+}
 
-    if (isset($_SESSION['update'])) {
-        echo $_SESSION['update'];
-        unset($_SESSION['update']);
-    }
-    if (isset($_SESSION['upload'])) {
-        echo $_SESSION['upload'];
-        unset($_SESSION['upload']);
-    }
+
+if (isset($_SESSION['no-user-found'])) {
+    echo $_SESSION['no-user-found'];
+    unset($_SESSION['no-user-found']);
+}
+
+if (isset($_SESSION['update'])) {
+    echo $_SESSION['update'];
+    unset($_SESSION['update']);
+}
+if (isset($_SESSION['upload'])) {
+    echo $_SESSION['upload'];
+    unset($_SESSION['upload']);
+}
 ?>
 <!doctype html>
 <html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/user.css">
-    <title>User Info</title>
-</head>
-
 <body>
-
+    <?php include('header.php'); ?>
 
     <div class="container bootstrap snippets bootdeys">
         <div class="row" id="user-profile">
-            
+
             <?php
 
             if (isset($_GET['id_user'])) {
@@ -55,7 +40,7 @@
                 $id_user = $_GET['id_user'];
 
 
-                $sql = "SELECT * FROM user WHERE id_user = $id_user";
+                $sql = "SELECT user.*, lop.ten_lop, lien_khoa.ten_LienKhoa from ((user INNER JOIN lop on  user.id_lop = lop.id_Lop) INNER JOIN lien_khoa ON user.id_LienKhoa = lien_khoa.id_LienKhoa) where id_user = '$id_user'" ;
 
                 $res = mysqli_query($conn, $sql);
 
@@ -69,7 +54,9 @@
                     $user_email = $row['user_email'];
                     $picture = $row['picture'];
                     $user_sdt = $row['user_sdt'];
-                    $id_lop = $row['id_lop'];
+                    $ten_lop = $row['ten_lop'];
+                    $ten_LienKhoa=$row['ten_LienKhoa'];
+                    $DOB =$row['DOB'];
                 } else {
 
                     header('location:' . SITEURL);
@@ -129,16 +116,32 @@
                             </div>
                             <div class="profile-user-details clearfix">
                                 <div class="profile-user-details-label">
-                                    Id Class:
+                                    Class:
                                 </div>
                                 <div class="profile-user-details-value">
-                                    <?php echo $id_lop; ?>
+                                    <?php echo $ten_lop; ?>
+                                </div>
+                            </div>
+                            <div class="profile-user-details clearfix">
+                                <div class="profile-user-details-label">
+                                    Niên Khóa:
+                                </div>
+                                <div class="profile-user-details-value">
+                                    <?php echo $ten_LienKhoa; ?>
+                                </div>
+                            </div>
+                            <div class="profile-user-details clearfix">
+                                <div class="profile-user-details-label">
+                                    Birthday:
+                                </div>
+                                <div class="profile-user-details-value">
+                                    <?php echo $DOB; ?>
                                 </div>
                             </div>
 
                         </div>
                     </div>
-                    <div class="container-fluid">
+                    <div class="container" style="padding: 0;">
                         <div class="row">
                             <div class="col-4">
                                 <div class="badge bg-primary text-wrap" style="width: 10rem; margin: 5% 20%; font-size: 20px;">
@@ -169,7 +172,7 @@
                                         <ul class="list-group ">
                                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                                 <?php echo $name_z; ?>
-                                                <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn btn-primary">Tham gia</a>
+                                                <a href="" class="btn btn-primary">Tham gia</a>
                                             </li>
                                         </ul>
 
@@ -215,7 +218,10 @@
 
                                             <tr>
                                                 <td><?php echo $sn++; ?>. </td>
-                                                <td><?php echo $sk_name; ?></td>
+                                                <td>
+                                                    <a href="<?php echo SITEURL; ?>user/eventdes.php?sk_id=<?php echo $sk_id; ?>" class="link-dark" style="text-decoration: none;"><?php echo $sk_name; ?> </a>
+
+                                                </td>
                                                 <td><?php echo $sk_date; ?></td>
                                                 <td>
                                                     <a href="<?php echo SITEURL; ?>user/invite.php?sk_id=<?php echo $sk_id; ?>" class="btn btn-primary">Mời</a>
@@ -241,10 +247,9 @@
 
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-
+    
+    
+    <?php include('footer.php'); ?>
 
 </body>
 
