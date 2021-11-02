0403f04018fc454f42101543f105795b4c58671d
Phạm Quang Huy <80248283+noohuy@users.noreply.github.com>
2021-11-02 23:28:49 +0700

    Upload header.php

---
+++ user/header.php
@@ -0,0 +1,52 @@
+<header>
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/user.css">
+    
+    <div class="px-3 py-2 bg-dark text-white">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <div  class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+            <h4 style =" margin:0;" >Diễn đàn cựu sinh viên CNTT</h4>
+          </div>
+
+          <ul class="nav col-12 col-lg-auto my-2 my-md-0 text-center">
+            <li>
+              <a href="#" class="nav-link text-white " style =" padding-top:0">
+                <svg class="bi d-block mx-auto "   width="10" height="10"><use xlink:href="#home"></use></svg>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white" style =" padding-top:0">
+                <svg class="bi d-block mx-auto " width="10" height="10"><use xlink:href="#speedometer2"></use></svg>
+                Event
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white" style =" padding-top:0">
+                <svg class="bi d-block mx-auto " width="10" height="10"><use xlink:href="#table"></use></svg>
+                Profile
+              </a>
+            </li>
+            <li>
+              <a href="index.php" class="nav-link text-white" style =" padding-top:0">
+                <svg class="bi d-block mx-auto " width="10" height="10"><use xlink:href="#grid"></use></svg>
+                Login
+              </a>
+            <li>
+              <a href="logout.php" class="nav-link text-white" style =" padding-top:0">
+                <svg class="bi d-block mx-auto " width="10" height="10"><use xlink:href="#grid"></use></svg>
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
